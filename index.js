173be0518bd498a4688961f38c94e297f11a397b
samda <muy.samda13@gmail.com>
2021-03-22 09:16:53 +0700

    prepare deployment

--- index.js
+++
@@ -1,6 +0,0 @@
-
-
-const navSlide = () => {
-  const bMenu = document.querySelector('#nav-toggle'); 
-  const nav = document.querySelector('.overlay-menu');
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
+const colors = require('tailwindcss/colors');
 module.exports = {
-  purge: [],
+  purge: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,ts,jsx,tsx}' ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      primary: {
+        light: '#f7b962',
+        DEFAULT: '#dc9d42',
+        dark: '#ba6f07'
+      },
+      black: colors.black,
+      white: colors.white,
+      gray: colors.trueGray,
+      indigo: colors.indigo,
+      red: colors.rose,
+      yellow: colors.amber
+    },
   },
-  variants: {
-    extend: {},
+ variants: {
+  extend: {},
+ },
+ 
+ extend: {
+   transitionDelay: {
+     '0': '0ms',
+     '2000': '2000ms',
+    },
   },
   plugins: [],
 }
