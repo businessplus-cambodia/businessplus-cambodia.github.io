c0db7eb6572f1bbe56808571817a9f0f83d31638
samda <muy.samda13@gmail.com>
2021-03-22 09:18:14 +0700

    chore(site): regen

---
+++ docs/_next/static/3dvoeq9pyRqIAV004gn0Z/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F801a6084.508e30dc738edecad176.js","static\u002Fchunks\u002Fpages\u002Findex-281009b769639bab5848.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ab816e36f4b0c62a39b8.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
